17c70ad731fa5c11f317907feb4f03f956e0a04d
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-04 16:44:27 +0530

    solution for day 4

---
+++ python/day4/day4_1.py
@@ -0,0 +1,70 @@
+inputs = []
+i = 0
+
+
+with open('input.txt') as fp:
+    for input in fp:
+        data = input.split(']')
+        datedata = long(data[0][1:5]+ data[0][6:8] + data[0][9:11] + data[0][12:14]+data[0][15:17])
+        if 'Guard' in input:
+            inputs.append([ datedata, {'id':data[1].split('#')[1].split('b')[0].strip()}])
+        else:
+            if 'asleep' in data[1]:
+                inputs.append([datedata, {'guard_status':0}])
+            if 'wakes' in data[1]:
+                inputs.append([datedata, {'guard_status':1}])
+        i = i + 1
+        
+inputs.sort(key=lambda x: x[0])
+guards = {}
+running_id = None
+last_slept_at = 0
+
+for data in inputs:
+    if 'id' in data[1]:
+        if data[1]['id'] not in guards:
+            guards[data[1]['id']] = {'totalsleep':0, 'sleptatminute':{}}
+        running_id = data[1]['id']
+        last_slept_at = 0
+    else:
+       if data[1]['guard_status'] == 0:
+           sleptat = str(data[0])[-2:]
+           last_slept_at = int(sleptat)
+           if sleptat not in guards[running_id]['sleptatminute']:
+               guards[running_id]['sleptatminute'][sleptat] = 1
+           else:
+               guards[running_id]['sleptatminute'][sleptat] += 1
+       if data[1]['guard_status'] == 1:
+          awakeat = int(str(data[0])[-2:])
+          guards[running_id]['totalsleep'] += awakeat - last_slept_at
+          for i in range(last_slept_at + 1, awakeat):
+            inpumin = str(i)
+            if inpumin not in guards[running_id]['sleptatminute']:
+               guards[running_id]['sleptatminute'][inpumin] = 1
+            else:
+               guards[running_id]['sleptatminute'][inpumin] += 1
+            
+
+           
+           
+max_sleep_minutes = 0
+guardid = 0
+for key, data in guards.iteritems():
+    if data['totalsleep'] > max_sleep_minutes:
+        guardid = key
+        max_sleep_minutes = data['totalsleep']
+        
+all_minutes = guards[guardid]['sleptatminute']
+max_slept_at = 0
+max_slept_at_minute = 0
+
+for key, data in all_minutes.iteritems():
+    if data > max_slept_at:
+        max_slept_at = data
+        max_slept_at_minute = key
+
+print('ANSWER')        
+print(guardid)
+print(max_slept_at_minute)
+print(int(guardid) * int(max_slept_at_minute))
+    
