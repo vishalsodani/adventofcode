2bf575cbd4c243678d22ba67102fa0b9273c4a4b
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-05 15:12:34 +0530

    remove a variable

--- python/day5/day5_2.py
+++ python/day5/day5_2.py
@@ -5,18 +5,16 @@ with open('input.txt') as fp:
         polymer = input
 
 
-az = string.ascii_lowercase[:26]   
+az = string.ascii_lowercase[:26]
 shortest_length = 0
 i = 0
 start_at = 'a'
 
 while start_at != 'z':
     last_character = ''
-    found = False
     no_match_found = False
     use_polymer = list(polymer.replace(start_at, '').replace(start_at.upper(), ''))
     while no_match_found == False:
-        found = False
         last_character = ''
         index_found = []
         for index, character in enumerate(use_polymer):
@@ -24,13 +22,12 @@ while start_at != 'z':
                 if character.isupper() and last_character.islower() or character.islower() and last_character.isupper():
                     index_found.append(index)
                     index_found.append(index-1)
-                    found = True
                     last_character = ''
             else:
                 last_character = character
         for ind in sorted(index_found, reverse=True):
             del use_polymer[ind]
-        if found == False:
+        if len(index_found) == 0:
             if shortest_length == 0:
                 shortest_length = len(use_polymer)
             elif len(use_polymer) < shortest_length:
