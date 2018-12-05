0a4f79a90aeea107a77001a800e6e18343190e18
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-05 20:11:21 +0530

    removed another variable

--- python/day5/day5_1.py
+++ python/day5/day5_1.py
@@ -7,7 +7,6 @@ no_match_found = False
 polymer = list(polymer)
 
 while no_match_found == False:
-    found = False
     next_input = []
     skip_index = -1
     for index, character in enumerate(polymer):
@@ -18,7 +17,6 @@ while no_match_found == False:
             next_character = polymer[index + 1]
             if character.lower() == next_character.lower():
                 if character.isupper() and next_character.islower() or character.islower() and next_character.isupper():
-                    found = True
                     skip_index = index + 1
                 else:
                     next_input.append(character)
@@ -26,9 +24,10 @@ while no_match_found == False:
                 next_input.append(character)
         except:
             next_input.append(character)
-    polymer = next_input
-    if found == False:
+    if polymer == next_input:
         break
+    polymer = next_input
+    
 print(len(polymer))
 
         
