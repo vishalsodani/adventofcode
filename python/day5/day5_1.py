b5dfb30ca2e3a0f9237ff18b807f43cc616ed1c7
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-05 20:07:44 +0530

    removed the mutation step and replaced with filter but skip_index looks ugly

--- python/day5/day5_1.py
+++ python/day5/day5_1.py
@@ -1,29 +1,32 @@
 polymer = ''
-import time
 with open('input.txt') as fp:
     for input in fp:
         polymer = input
 
-last_character = ''
 no_match_found = False
 polymer = list(polymer)
 
 while no_match_found == False:
     found = False
-    last_character = ''
-    index_found = []
+    next_input = []
+    skip_index = -1
     for index, character in enumerate(polymer):
-        if character.lower() == last_character.lower():
-            if character.isupper() and last_character.islower() or character.islower() and last_character.isupper():
-                index_found.append(index)
-                index_found.append(index-1)
-                found = True
-                last_character = ''
-                
-        else:
-            last_character = character
-    for ind in sorted(index_found, reverse=True):
-        del polymer[ind]
+        if index == skip_index:
+            skip_index = -1
+            continue
+        try:
+            next_character = polymer[index + 1]
+            if character.lower() == next_character.lower():
+                if character.isupper() and next_character.islower() or character.islower() and next_character.isupper():
+                    found = True
+                    skip_index = index + 1
+                else:
+                    next_input.append(character)
+            else:
+                next_input.append(character)
+        except:
+            next_input.append(character)
+    polymer = next_input
     if found == False:
         break
 print(len(polymer))
