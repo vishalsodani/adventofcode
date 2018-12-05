7a7b63f305df33d2b8f76b1787fc74bc8cbd988b
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-05 13:12:13 +0530

    improved and faster; removed unnecessary loop

--- python/day5/day5_1.py
+++ python/day5/day5_1.py
@@ -19,7 +19,6 @@ while no_match_found == False:
             break
         if character.lower() == last_character.lower():
             if character.isupper() and last_character.islower() or character.islower() and last_character.isupper():
-                #import pdb;pdb.set_trace()
                 new_str = polymer[index+1:]
                 old_str = polymer[0:index-1]
                 polymer = old_str + new_str
@@ -31,19 +30,6 @@ while no_match_found == False:
     match_found = False
     if found == False:
         break
-    #import pdb;pdb.set_trace()
-    for index, character in enumerate(polymer):
-        if character.lower() == last_character_in.lower():
-            if character.isupper() and last_character.islower() or character.islower() and last_character.isupper():
-                match_found = True
-                break
-        else:
-            last_character_in = character
-            match_found = False
-    if match_found == False:
-        no_match_found = True
-    #if match_found:
-        #break
 print(len(polymer))
         
      
