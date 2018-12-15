6978d7780c66270f41db6d9fb37dafe149464978
Vishal Sodani <vishalsodani@rediffmail.com>
2018-12-15 12:11:06 +0530

    fixing this solution after finding solution with rust

--- python/day11/day11_2.py
+++ python/day11/day11_2.py
@@ -1,12 +1,12 @@
-grid_serial_number = 42
-range_x = 5
-range_y = 5
+grid_serial_number = 9221
+range_x = 300
+range_y = 300
 fuel_grid = [[0 for x in range(range_x)] for y in range(range_y)]
 
 total_cells_with_neg_power = 0
 positive_power = 0
 top_left_fuel = []
-size_of_square = 1
+size_of_square = 9
 grid_size = 0
 
 for i in range(0, range_x):
@@ -24,18 +24,15 @@ for i in range(0, range_x):
         power_level = hundreds - 5
         fuel_grid[i][y] = power_level
 
-while size_of_square < range_x + 1:
+while size_of_square < 13:
     for i in range(0, range_x):
         if size_of_square > 1 and i + size_of_square > range_x:
             continue
         for y in range(0, range_y ):
             if size_of_square > 1 and y + size_of_square > range_y:
                 continue
-
-            
             power_square = 0        
             for dx in range(i, i+size_of_square):
-                
                 for ey in range(y, y+size_of_square):
                     if (ey <= range_x - 1) and (dx <= range_y - 1):
                         power_square += fuel_grid[dx][ey]
